dac29a8aaecc7b9427a5063413c8321b29650f12
pstamim <pstamim723@gmail.com>
2026-09-23 01:39:27 +0600

    PROBLEM FACE

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -11,7 +11,7 @@ interface Iparamsprops {
 }
 
 const getbooks = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_UR}/booksdata.json`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksdata.json`)
     const data = await res.json()
     return data
 }
