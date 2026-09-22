b467ec0f4eedc363c0842d12c122da7d10373e39
pstamim <pstamim723@gmail.com>
2026-09-22 23:39:58 +0600

    env added

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -11,7 +11,7 @@ interface Iparamsprops {
 }
 
 const getbooks = async () => {
-    const res = await fetch('http://localhost:3000/booksdata.json')
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_UR}/booksdata.json`)
     const data = await res.json()
     return data
 }
